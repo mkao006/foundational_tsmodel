b8b2cbef5b687f0437e2534e464dfc8cc136cb39
Michael C. J. Kao <mkao006@gmail.com>
2024-05-26 19:49:15 +0800

    Update with Evaluator and added Large chronos model

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
-import torch
-from chronos import ChronosPipeline
-from datasetsforecast.m3 import M3
+import json
+
+import pandas as pd
+from darts.metrics.metrics import smape
+from darts.timeseries import TimeSeries
 from datasetsforecast.m4 import M4
 
+from evaluator import Evaluator
 from model import ChronosFoundationalModel, ForecastParam
 
-DATASET_DIRECTORY = "datasets"
-GROUP = "Monthly"
+DATASET_DIRECTORY = 'datasets'
+GROUP = 'Monthly'
 
 # Get the data
-m3_datasets = M3.load(directory=DATASET_DIRECTORY, group=GROUP)[0]
 m4_datasets = M4.load(directory=DATASET_DIRECTORY, group=GROUP)[0]
 
 chronos_params = {
-    "training_params": {},
-    "prediction_params": {"prediction_length": 12, "num_samples": 20},
+    'training_params': {},
+    'prediction_params': {'prediction_length': 12, 'num_samples': 20},
 }
 
 chronosFMSmall = ChronosFoundationalModel(
-    ChronosPipeline.from_pretrained(
-        "amazon/chronos-t5-small",
-        # NOTE (Michael): using cpu instead of mps as some dependency are not
-        #                 available on apple silicon.
-        device_map="cpu",
-        torch_dtype=torch.bfloat16,
-    ),
-    ForecastParam(**chronos_params),
+    pretrained_model_name_or_path='amazon/chronos-t5-small',
+    params=ForecastParam(**chronos_params),
 )
 
-cp = chronosFMSmall.predict(m4_datasets)
 
+chronosFMLarge = ChronosFoundationalModel(
+    pretrained_model_name_or_path='amazon/chronos-t5-large',
+    params=ForecastParam(**chronos_params),
+)
 
-if __name__ == "__main__":
-    pass
+# TODO (Michael): Write an adaptor decorator for Darts metric.
+def smape_metric(actual=pd.Series, predicted=pd.Series) -> float:
+    return smape(TimeSeries.from_series(actual), TimeSeries.from_series(predicted))
+
+evaluator = Evaluator(
+    data=m4_datasets,
+    models=[
+        chronosFMSmall,
+        chronosFMLarge,
+    ],
+    metric=smape_metric,
+    sample_size=10,
+    train_window_size=12,
+    test_window_size=12,
+)
+
+result = evaluator.evaluate()
+print(json.dumps(result, indent=4))
