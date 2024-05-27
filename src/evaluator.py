c91db19d7e134922189edef2fd6636dd8d5a11b1
Michael C. J. Kao <mkao006@gmail.com>
2024-05-27 08:33:51 +0800

    Add documentation

--- src/evaluator.py
+++ src/evaluator.py
@@ -20,6 +20,10 @@ class Evaluator:
         train_window_size: int = 12,
         test_window_size: int = 12,
     ):
+        """The valuator function performs benchmark using the data and metrics
+        provided for each model.
+
+        """
         self.data = TSDataSchema.validate(data)
         self.models = models
         self.metric = metric
@@ -29,6 +33,7 @@ class Evaluator:
         self.sampled_df = None
 
     def evaluate(self) -> Dict:
+        """Benchmark the model performance"""
         if self.sampled_df is None:
             self._generate_sample()
 
@@ -46,6 +51,15 @@ class Evaluator:
         return result
 
     def _generate_sample(self):
+        """Function to generate the cross-validation set. We first sample the
+        unique_id which is the ID representing the time series, then for each
+        time series, we sample a subset of a time series then split into
+        training and test set.
+
+        The output is a list of length n equivalent to the sample size, and
+        each element is a tuples which contains the train and test set.
+
+        """
         uid = self.data[TSDataSchema.unique_id].unique()
         sampled_ids = np.random.choice(uid, self.sample_size, replace=True)
         self.sampled_df = [
@@ -60,6 +74,10 @@ class Evaluator:
     def _split_train_test(
         self, df: pd.DataFrame, train_window_size: int, test_window_size: int
     ) -> pd.DataFrame:
+        """Given a full time series, sample a subset of the time series and
+        split into training and test set.
+
+        """
         if df[TSDataSchema.unique_id].nunique() > 1:
             raise ValueError(
                 """ Multiple 'unique_id' found in the data frame, the
@@ -86,4 +104,6 @@ class Evaluator:
 
 # TODO (Michael): Write an adaptor decorator for Darts metric.
 def smape_metric(actual=pd.Series, predicted=pd.Series) -> float:
+    """Convert the sMAPE from Dart to be compatible with the Evaluator
+    interface"""
     return smape(TimeSeries.from_series(actual), TimeSeries.from_series(predicted))
